d7bafd056d2f15bc6de43882b9a876c950fd1fbd
xuan.z <111435889+xzh976@users.noreply.github.com>
2022-12-06 16:05:12 +0800

    Update train_Sony.py

--- deep_denoising/train_Sony.py
+++ deep_denoising/train_Sony.py
@@ -64,7 +64,6 @@ def generate_noisy_raw(gt_raw, a, b, fpn):
     poisson_noisy_img = poisson(np.maximum(gt_raw-512, 0) / a).rvs() * a
     poisson_fpn = poisson(fpn).rvs()
     gaussian_noise = np.sqrt(gaussian_noise_var) * np.random.randn(gt_raw.shape[0], gt_raw.shape[1])
-    # gaussian_noise = norm(loc=np.zeros(gaussian_noise_var), scale=np.sqrt(gaussian_noise_var)).rvs()
     noisy_img = poisson_noisy_img + gaussian_noise + poisson_fpn + 512
     noisy_img = np.minimum(np.maximum(noisy_img, 0), 2 ** 14 - 1)
 
