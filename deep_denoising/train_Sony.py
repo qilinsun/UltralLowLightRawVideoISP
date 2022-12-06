64920706b06f3c3328f0a3c7c5138210151e9f11
xuan.z <111435889+xzh976@users.noreply.github.com>
2022-12-06 16:04:41 +0800

    Update train_Sony.py

--- deep_denoising/train_Sony.py
+++ deep_denoising/train_Sony.py
@@ -55,6 +55,21 @@ if DEBUG == 1:
 def load_fpn(fpn_path):
     return scipy.io.loadmat(fpn_path)["mean_pattern"]
 
+def generate_noisy_raw(gt_raw, a, b, fpn):
+    """
+    a: sigma_s^2
+    b: sigma_r^2
+    """
+    gaussian_noise_var = b
+    poisson_noisy_img = poisson(np.maximum(gt_raw-512, 0) / a).rvs() * a
+    poisson_fpn = poisson(fpn).rvs()
+    gaussian_noise = np.sqrt(gaussian_noise_var) * np.random.randn(gt_raw.shape[0], gt_raw.shape[1])
+    # gaussian_noise = norm(loc=np.zeros(gaussian_noise_var), scale=np.sqrt(gaussian_noise_var)).rvs()
+    noisy_img = poisson_noisy_img + gaussian_noise + poisson_fpn + 512
+    noisy_img = np.minimum(np.maximum(noisy_img, 0), 2 ** 14 - 1)
+
+    return noisy_img
+
 def pack_raw(raw):
     #pack Bayer image to 4 channels
     im = raw.raw_image_visible.astype(np.float32) 
